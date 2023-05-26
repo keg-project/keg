d5693d8bc1932594632fea6b6cfec3a2d6d95a21
Xiang Fan <sfanxiang@gmail.com>
2023-05-26 19:51:37 -0400

    Add tests for keg-user

--- tests/tests.sh
+++ tests/tests.sh
@@ -16,10 +16,14 @@ cargo run --bin keg-base -- $KEG_BASE_TEST_ARGS -- /bin/true
 ! cargo run --bin keg-base -- $KEG_BASE_TEST_ARGS -- /bin/false
 cargo run --bin keg-rootfs -- -u target/test_container $KEG_ROOTFS_TEST_ARGS -- /bin/true
 ! cargo run --bin keg-rootfs -- -u target/test_container $KEG_ROOTFS_TEST_ARGS -- /bin/false
+
 cargo run --bin keg -- $KEG_TEST_ARGS -- sh tests/include/true.sh
 ! cargo run --bin keg -- $KEG_TEST_ARGS -- sh tests/include/false.sh
 cargo run --bin keg-home -- $KEG_HOME_TEST_ARGS -- sh tests/include/true.sh
 ! cargo run --bin keg-home -- $KEG_HOME_TEST_ARGS -- sh tests/include/false.sh
+cargo run --bin keg-user -- $KEG_USER_TEST_ARGS -- sh tests/include/true.sh
+! cargo run --bin keg-user -- $KEG_USER_TEST_ARGS -- sh tests/include/false.sh
+
 cargo run --bin keg-rootfs -- -u target/test_container $KEG_ROOTFS_TEST_ARGS -- /bin/sh -c "printf '#!/bin/sh\ntrue\n' > /usr/local/bin/mytrue && chmod +x /usr/local/bin/mytrue"
 cargo run --bin keg-rootfs -- -u target/test_container $KEG_ROOTFS_TEST_ARGS -- /bin/sh -c "printf '#!/bin/sh\nfalse\n' > /usr/local/bin/myfalse && chmod +x /usr/local/bin/myfalse"
 cargo run --bin keg-rootfs -- -u target/test_container $KEG_ROOTFS_TEST_ARGS -- /bin/sh -c mytrue
